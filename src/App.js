a4e7a40aef9f7ba669a9abeb2fc4ece8b8d22472
Patrick Toepel <ptoepel@outsource-consultants.com>
2023-03-02 16:03:12 -0600

    redundent alt fix

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ return (
             <br />
             {profile ? (
                 <div>
-                    <img src={profile.picture} alt="user image" />
+                    <img src={profile.picture}/>
                     <h3>User Logged in</h3>
                     <p>Name: {profile.name}</p>
                     <p>Email Address: {profile.email}</p>
